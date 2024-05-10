f9d6033571a1163e1e153d1b96cf7b84de40e1e1
Rachel Opuba <ogbenirach@gmail.com>
2024-05-10 18:02:02 +0100

    Fixed deployment issues with escaping apostophe

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,13 +14,13 @@ export default function NotFound() {
           </Button>
           <div className="text-[#030b1d]  text-center px-4 flex flex-col justify-center items-center font-medium">
             <h1 className="text-[36px] lg:text-[45px] xl:text-[50px]  leading-[112%] pt-4">
-              Why wasn't this <span className="text-[#0357ee]">page</span>{" "}
+              Why wasn&lsquo;t this <span className="text-[#0357ee]">page</span>{" "}
               found?
             </h1>
             <p className="py-4  max-w-[500px] lg:max-w-[550px]  xl:max-w-[650px] text-center w-full leading-[156%] text-base lg:text-lg">
-              It would seem that the page you’re looking for is out on loan,
-              powering dreams for global businesses out there. Let’s find you a
-              page that’s still here.
+              It would seem that the page you&lsquo;re looking for is out on loan,
+              powering dreams for global businesses out there. Let&lsquo;s find you a
+              page that&lsquo;s still here.
             </p>
           </div>
           <Link href={"/"}>
